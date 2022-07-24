bf64ae93d67bc16694096faa84fb5e7e1387cf86
Scott Hallock <scott@hallocks.net>
2022-07-24 15:40:48 +0000

    Fix database connection

--- data/database.js
+++ data/database.js
@@ -1,22 +1,29 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connect() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('blog');
+  if (!process.env.MONGODB_USERNAME || !process.env.MONGODB_PASSWORD) {
+    console.error("Set MONGODB_USERNAME and MONGODB_PASSWORD!");
+    process.exit(-1);
+  }
+
+  const client = await MongoClient.connect(
+    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongo`
+  );
+  database = client.db("blog");
 }
 
 function getDb() {
   if (!database) {
-    throw { message: 'Database connection not established!' };
+    throw { message: "Database connection not established!" };
   }
   return database;
 }
 
 module.exports = {
   connectToDatabase: connect,
-  getDb: getDb
+  getDb: getDb,
 };
