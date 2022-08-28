ed537fd3bd86f843df020b3c6dc36af57c706b92
Scott Hallock <scott@hallocks.net>
2022-08-28 16:35:34 +0000

    Gather entered comment data and log it

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,5 +1,8 @@
 const loadCommentsBtnElement = document.querySelector("#load-comments-btn");
 const commentsSectionElement = document.querySelector("#comments");
+const commentsFormElement = document.querySelector("#comments-form form");
+const commentTitleElement = document.querySelector("#title");
+const commentTextElement = document.querySelector("#text");
 
 const renderCommentsList = (comments) => {
   const commentListElement = document.createElement("ol");
@@ -23,4 +26,13 @@ loadCommentsBtnElement.addEventListener("click", async () => {
   const commentsListElement = renderCommentsList(responseData);
   commentsSectionElement.innerHTML = "";
   commentsSectionElement.appendChild(commentsListElement);
+});
+
+commentsFormElement.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  console.log({ title: enteredTitle, text: enteredText });
 });
