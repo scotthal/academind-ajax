c4ae2f1c390aebf3400c8e9bd723faa77b9986ef
Scott Hallock <scott@hallocks.net>
2022-08-28 16:22:53 +0000

    Render fetched comment data into post details page

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,8 +1,26 @@
 const loadCommentsBtnElement = document.querySelector("#load-comments-btn");
+const commentsSectionElement = document.querySelector("#comments");
+
+const renderCommentsList = (comments) => {
+  const commentListElement = document.createElement("ol");
+  for (const comment of comments) {
+    const commentElement = document.createElement("li");
+    commentElement.innerHTML = `
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+    `;
+    commentListElement.appendChild(commentElement);
+  }
+  return commentListElement;
+};
 
 loadCommentsBtnElement.addEventListener("click", async () => {
   const postId = loadCommentsBtnElement.dataset.postid;
   const res = await fetch(`/posts/${postId}/comments`);
   const responseData = await res.json();
-  console.log(responseData);
+  const commentsListElement = renderCommentsList(responseData);
+  commentsSectionElement.innerHTML = "";
+  commentsSectionElement.appendChild(commentsListElement);
 });
